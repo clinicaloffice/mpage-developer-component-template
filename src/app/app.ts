7037bfc2b62c6e2ec7e1979da7bbc6bc1bbd81d1
John Simpson <jsimpson@precisionhealth.net>
2026-05-30 16:40:12 -0400

    Version 5.1 Updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -21,7 +21,7 @@ declare const VERSION: string;
   imports: [MpageLogComponent],
   templateUrl: './app.html',
   standalone: true,
-  styleUrls: ['../styles.scss', '../clinical-office-styles.scss'],
+  styleUrls: ['../styles.scss', '../../node_modules/@clinicaloffice/mpage-developer/styles/clinical-office-styles.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.ShadowDom,
   providers: [MPageService, MPageLogService, AddressService, AllergyService, CodeValueService, ConfigService, CustomService,
@@ -34,16 +34,18 @@ export class App implements OnInit {
 
   public title: InputSignal<string> = input('default');
   public path: InputSignal<string> = input('path');
-  public person_id: InputSignal<number> = input(-1);
-  public encntr_id: InputSignal<number> = input(-1);
-  public prsnl_id: InputSignal<number> = input(-1);
+  public person_id: InputSignal<string> = input('');
+  public encntr_id: InputSignal<string> = input('');
+  public prsnl_id: InputSignal<string> = input('');
+  public disable_debugger: InputSignal<string> = input('0');
 
   ngOnInit() {
     // Collect any parameters in the URL (Used in Cerner Components)
     this.activatedRoute.queryParams.subscribe(params => {
-      this.MPage.personId = params['personId'] ? parseInt(params['personId']) : this.person_id() !== -1 ? this.person_id() : this.MPage.personId;
-      this.MPage.encntrId = params['encounterId'] ? parseInt(params['encounterId']) : this.encntr_id() !== -1 ? this.encntr_id() : this.MPage.encntrId;
-      this.MPage.prsnlId = params['userId'] ? parseInt(params['userId']) : this.prsnl_id() !== -1 ? this.prsnl_id() : this.MPage.prsnlId;
+      this.MPage.personId = params['personId'] ? parseInt(params['personId']) : this.person_id() !== '' ? parseInt(this.person_id()) : this.MPage.personId;
+      this.MPage.encntrId = params['encounterId'] ? parseInt(params['encounterId']) : this.encntr_id() !== '' ? parseInt(this.encntr_id()) : this.MPage.encntrId;
+      this.MPage.prsnlId = params['userId'] ? parseInt(params['userId']) : this.prsnl_id() !== '' ? parseInt(this.prsnl_id()) : this.MPage.prsnlId;
+      this.MPage.disableDebugger = parseInt(this.disable_debugger()) === 1;
     });
 
     this.MPage.setMaxInstances(2, true, 'CHART', false);
